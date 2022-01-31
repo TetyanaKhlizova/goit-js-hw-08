eaf73efac0b4434f95638948f191d54c594f1247
TetyanaKhlizova <90279592+TetyanaKhlizova@users.noreply.github.com>
2022-01-31 23:17:12 +0200

    start2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const containerGallery = document.querySelector('ul.gallery');
+const htmlGallery = galleryItems.reduce((acc, {preview,  original, description}) => {
+    acc += `<li> <a class="gallery__item" href='${original}'>
+  <img class="gallery__image"
+   src='${preview}',
+    alt='${description}', />
+    </a> </Li> `;
+    return acc;
+},'');
+containerGallery.insertAdjacentHTML('beforeend', htmlGallery);
+containerGallery.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    console.log(event.target);
+    
+      
+}
+let lightbox = new SimpleLightbox('.gallery a ', {caphionsDelay: 250, captionsData: 'alt'});
+
 console.log(galleryItems);
