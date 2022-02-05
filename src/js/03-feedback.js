b1cff5f96febcfd2f1b83e2be19a5f2be5fbc378
TetyanaKhlizova <90279592+TetyanaKhlizova@users.noreply.github.com>
2022-02-05 15:55:32 +0200

    check

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,6 +5,8 @@
 //  При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
 //  При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
 //  Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+const formData = {};
+const LOC_KEY = 'feedback-form-state';
 
 const refs = {
     form: document.querySelector('.feedback-form'), 
@@ -12,8 +14,20 @@ const refs = {
     textarea: document.querySelector('.feedback-form textarea'),
 }
 refs.form.addEventListener('submit', onFormSubmit);
-refs.input.addEventListener('input', throttle(onEmailInput, 500));
-refs.textarea.addEventListener('input',  throttle(onTextAreaInput,500));
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+// refs.input.addEventListener('input', throttle(onEmailInput, 500));
+// refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
+ function onFormInput({ name, value }) {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(LOC_KEY, JSON.stringify(formValues));
+    
+};
+
+
+
+
 
 populateTextarea();
 populateEmail();
@@ -50,82 +64,8 @@ function populateTextarea() {
         refs.textarea.value = savedMsg;
     }
     
-}
+    };
 
 
 
 
-// const output = document.querySelector("#output");
-// const LOCALSTORAGE_KEY = "goit-example-message";
-
-// updateOutput();
-// form.addEventListener("submit", saveMessage);
-
-// function saveMessage(evt) {
-//   evt.preventDefault();
-//   localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
-//   updateOutput();
-//   form.reset();
-// }
-
-// function updateOutput() {
-//   output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// const form = document.querySelector('.feedback-form');
-
-// const LOCAL_KEY = 'feedback-form-state';
-
-// addLocalData();
-
-// const formValues = {
-//   email: form.email.value,
-//   message: form.message.value,
-// };
-
-// form.addEventListener('input', throttle(onFormInput, 500));
-// form.addEventListener('submit', onFormSubmit);
-
-// function onFormInput({ target: { name, value } }) {
-//   formValues[name] = value;
-//   localStorage.setItem(LOCAL_KEY, JSON.stringify(formValues));
-// }
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-
-//   const submittedData = {
-//     email: event.currentTarget.email.value,
-//     message: event.currentTarget.message.value,
-//   };
-
-//   if (submittedData.email === '' || submittedData.message === '') {
-//     alert('Please fill all fields!');
-//     return;
-//   }
-
-//   console.log(submittedData);
-
-//   localStorage.removeItem(LOCAL_KEY);
-//   form.reset();
-// }
-
-// function addLocalData() {
-//   const localData = JSON.parse(localStorage.getItem(LOCAL_KEY));
-
-//   if (!localData) return;
-
-//   if (localData.email) form.email.value = localData.email;
-//   if (localData.message) form.message.value = localData.message;
-// }
